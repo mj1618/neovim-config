ade6a09e976173aa21afb5b0cf99c342135a63a9
Matt James <mj@Matts-MacBook-Air.local>
2024-08-07 13:10:53 +0800

    ignore folders

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-require('telescope').setup{ 
-  defaults = { 
-    file_ignore_patterns = { 
-      "node_modules" 
-    }
-  }
-}
-
+require("telescope").setup({
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      "node_modules",
+      "dist",
+      "build",
+    },
+  },
+})
